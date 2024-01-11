c5968bef720323d0171df3ccba4fec0d7e7bbe87
David <126608884+davidfjr@users.noreply.github.com>
2024-01-11 20:45:45 -0300

    Comments and Refactoring
    Added comments and madea refactoring in the modal

--- script.js
+++ script.js
@@ -6,34 +6,17 @@ const modal = document.querySelector("#modal")
 const openModal = document.querySelector("#open--modal")
 const closeModal = document.querySelector(".close--modal")
 const overlay = document.querySelector(".overlay")
+const toggleModalBtns = [openModal, closeModal, overlay]
 
-
+// OPEN JSON FILE
 fetch('./data.json')
     .then((response) => response.json())
     .then((json) => {
         data = json
-        init()
+        init() // initializes the application after the data is fetched
     });
 
-function marker(phrase) {
-    const splittedWords = phrase.replaceAll(regex, '')
-        .toLowerCase()
-        .split(" ")
-
-    const finalList = []
-
-    splittedWords.forEach(word => {
-        if (data[word]) {
-            finalList.push(data[word])
-        } else {
-            finalList.push(word)
-        }
-    });
-
-    output.value = finalList.toString().replaceAll(",", ' ')
-}
-
-
+// INITIALIZE APLICATION
 function init() {
     btn.addEventListener("click", () => {
         phrase = input.value
@@ -41,23 +24,36 @@ function init() {
     })
 }
 
+// SPLITTING THE INDIVIDUAL WORDS
+function marker(phrase) {
+    const splittedWords = phrase.replaceAll(regex, '') // removes special characters
+        .toLowerCase() // turns everything to lower case to make it possible to search in the database
+        .split(" ") // takes the words between spaces and add to an array
 
+    // FINDING THE WORD IN THE DATABASE
+    const finalList = []
+    splittedWords.forEach(word => {
+        if (data[word]) { // looks for the key in the JSON file with the same name of the word
+            finalList.push(data[word]) // if the key is in the file, its value is added to the array above
+        } else {
+            finalList.push(word) // if the key is not there, the word is added to array as it is
+        }
+    });
+
+    // OUTPUTTING THE WORDS WITH ACCENT
+    output.value = finalList.toString().replaceAll(",", ' ') // converts the array into a string and changes the comma for spaces
+}
+
+
+// TOGGLE MODAL
 function toggleModal() {
     modal.classList.toggle("hidden")
     overlay.classList.toggle("hidden")
 }
 
-openModal.addEventListener("click", () => {
+toggleModalBtns.forEach(btn => btn.addEventListener("click", () => {
     toggleModal()
-})
-
-closeModal.addEventListener("click", () => {
-    toggleModal()
-})
-
-overlay.addEventListener("click", () => {
-    toggleModal()
-})
+}))
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Escape") {
